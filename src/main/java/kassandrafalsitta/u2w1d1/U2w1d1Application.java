331925880d12a0d679a8d623c2534eaaf42a183a
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-09-02 13:23:15 +0200

    create repository

---
+++ src/main/java/kassandrafalsitta/u2w1d1/U2w1d1Application.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.u2w1d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w1d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w1d1Application.class, args);
+	}
+
+}
